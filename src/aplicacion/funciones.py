d91eba417b683814b9cce7d7a42b8604a621e568
ifh1001 <74533930+ifh1001@users.noreply.github.com>
2022-06-19 20:29:19 +0200

    Corregida la Nueva Técnica de Cálculo de la Longitud del Diente + Implementación en la Aplicación

--- src/aplicacion/funciones.py
+++ src/aplicacion/funciones.py
@@ -17,6 +17,7 @@ from detectron2.config import get_cfg
 from detectron2 import model_zoo
 from detectron2.utils.visualizer import Visualizer, ColorMode
 from skimage.morphology import thin
+from scipy.spatial import distance
 
 
 # -- Clases --
@@ -42,8 +43,8 @@ def aplicacion(uploader):
         imagen_bytes = Image.open(io.BytesIO(file_info['content']))
         imagen = cv2.cvtColor(np.array(imagen_bytes), cv2.COLOR_RGB2BGR)
         
-        puntoA, puntoB, distancia, im_predicc = calcula_distancia(imagen, predictor)
-        muestra_imagenes(imagen, im_predicc, puntoA, puntoB)
+        puntoA, puntoB, distancia, im_predicc, puntos = calcula_distancia(imagen, predictor)
+        muestra_imagenes(imagen, im_predicc, puntoA, puntoB, puntos)
         muestra_distancia(distancia)
 
 
@@ -86,10 +87,13 @@ def obtiene_configuracion():
 #  - PuntoA: punto de la forma (y,x) de la parte superior del diente.
 #  - PuntoB: punto de la forma (y,x) de la parte inferior del diente.
 #  - distancia: distancia entre el puntoA y el puntoB.
+#  - im_predicc: imagen con la predicción del Detectron2.
+#  - puntos: puntos de intersección entre el thin y las rectas perpendiculares.
 def calcula_distancia(imagen, predictor):
     predicciones = predictor(imagen)
     resolucion_y = 41 / imagen.shape[0]
     resolucion_x = 31 / imagen.shape[1]
+    resolucion = 41 / imagen.shape[0]
 
     diente, nervio = ordena_predicciones(predicciones)
 
@@ -112,9 +116,7 @@ def calcula_distancia(imagen, predictor):
     puntoB = y, x
 
     # Cálculo de la distancia entre los puntos
-    xx = ((puntoA[1] - puntoB[1]) * resolucion_x) ** 2
-    yy = ((puntoA[0] - puntoB[0]) * resolucion_y) ** 2
-    distancia = (xx + yy) ** 0.5
+    distancia, puntos = distancia_entre_partes(nervio_thin, puntos_mitad, puntoA, puntoB)
     
     #Preparamos imagen con las predicciones
     v = Visualizer(imagen[:, :, ::-1],
@@ -122,8 +124,102 @@ def calcula_distancia(imagen, predictor):
                    instance_mode=ColorMode.IMAGE_BW)
     
     im_predicc = v.draw_instance_predictions(predicciones["instances"].to("cpu"))
-    return puntoA, puntoB, distancia, im_predicc
+    return puntoA, puntoB, distancia * resolucion, im_predicc, puntos
 
+
+# Método encargado de calcular la longitud del diente, junto con los distintos
+# puntos del nervio sobre los que se cálcula la distancia.
+#
+# Parámetros:
+#  - nervio_thin: máscara del nervio obtenida con el thin.
+#  - puntos_mitad: puntos de la máscara thin.
+#  - puntoA: punto de la forma (y,x) de la parte superior del diente.
+#  - puntoB: punto de la forma (y,x) de la parte inferior del diente.
+# Return:
+#  - distancia: longitud total del diente.
+#  - puntos: puntos de intersección entre el thin y las rectas perpendiculares.
+def distancia_entre_partes(nervio_thin, puntos_mitad, puntoA, puntoB):
+    punto_inicio = (puntos_mitad[0][0], puntos_mitad[1][0])
+    punto_fin = (puntos_mitad[0][-1], puntos_mitad[1][-1])
+    
+    # Cálculo pendiente
+    pendiente = (puntoB[0] - puntoA[0]) / (puntoB[1] - puntoA[1]) 
+    
+    # Obtenciíon recta perpendicular
+    pend_perpendicular = - 1 / pendiente    
+    constante = -(pend_perpendicular * punto_inicio[1]) + punto_inicio[0]    
+    
+    #Puntos de intersección entre recta perpendicular y el thin del diente
+    puntos = dibuja_perpendiculares(pend_perpendicular, constante, punto_inicio, punto_fin, puntos_mitad)
+    
+    # Cálculo distancia
+    puntos.append(punto_fin)
+    distancia = distancia_entre_puntos(puntos)    
+    distancia = distancia + distance.euclidean((punto_inicio[1], punto_inicio[0]), (puntoA[1], puntoA[0]))
+    distancia = distancia + distance.euclidean((punto_fin[1], punto_fin[0]), (puntoB[1], puntoB[0]))    
+    
+    return distancia, puntos
+
+
+# Método encargado de calcular la longitud entre cada par de puntos.
+#
+# Parámetros:
+#  - puntos: puntos de intersección entre el thin del nervio y las rectas
+#            perpendiculares.
+# Return:
+#  - distancia: longitud total entre los puntos.
+def distancia_entre_puntos(puntos):
+    distancia = 0
+    for i in range(len(puntos)-1):
+        punto1 = puntos[i]
+        punto2 = puntos[i+1]
+        distancia = distancia + distance.euclidean((punto1[1], punto1[0]), (punto2[1], punto2[0]))
+        
+    return distancia
+
+
+# Método encargado de obtener los puntos de corte entre las rectas
+# perpendiculares y la máscara thin.
+#
+# Parámetros:
+#  - pend_perpendicular: pendiente de la recta perpendicular.
+#  - constante: constante de la recta perpendicular que pasa por el primer
+#               punto.
+#  - punto_inicio: primer punto de la máscara thin.
+#  - punto_fin: último punto de la máscara thin.
+#  - puntos_thin: todos los puntos de las máscara thin del nervio.
+# Return:
+#  - lista_puntos: lista con todos los puntos de intersección.
+def dibuja_perpendiculares(pend_perpendicular, constante, punto_inicio, punto_fin, puntos_thin):
+    divisiones = 20    
+    distancia = (punto_fin[0] - punto_inicio[0]) / divisiones
+    lista_puntos = []    
+    
+    for i in range(divisiones):    
+        lista_puntos.append(comprueba_puntos(puntos_thin, pend_perpendicular, constante + distancia*i))
+    
+    return lista_puntos
+
+
+# Método encargado de obtener el punto de intersección entre una recta y la
+# máscara thin.
+#
+# Parámetros:
+#  - puntos_thin: todos los puntos de las máscara thin del nervio.
+#  - pend_perpendicular: pendiente de la recta perpendicular.
+#  - constante: constante de la recta perpendicular que pasa por el primer
+#               punto.
+# Return:
+#  - punto: punto de la forma (y,x) de la intersección.
+def comprueba_puntos(puntos_thin, pend_perpendicular, constante):    
+    for i in range(len(puntos_thin[1])):        
+        y = pend_perpendicular*puntos_thin[1][i] + constante
+        if int(y) == puntos_thin[0][i]:            
+            return (int(y), puntos_thin[1][i])
+            
+        if int(y)+1 == puntos_thin[0][i]:            
+            return (int(y)+1, puntos_thin[1][i])
+        
 
 # Método encargado de ordenar las predicciones.
 #
@@ -198,9 +294,12 @@ def punto_correcto(diente, punto1, punto2, y, superior=True):
 #
 # Parámetros:
 #  - imagen: imagen con la que se quiere trabajar.
+#  - im_predicc: imagen con la predicción del Detectron2.
 #  - puntoA: primer punto de la recta.
 #  - puntoB: segundo punto de la recta.
-def muestra_imagenes(imagen, im_predicc, puntoA, puntoB):
+#  - puntos: puntos de intersección entre el thin del nervio y las rectas
+#            perpendiculares. 
+def muestra_imagenes(imagen, im_predicc, puntoA, puntoB, puntos):
     fig = plt.figure()
     fig.set_size_inches(14, 22)
     fig.add_subplot(1, 3, 1)
@@ -216,7 +315,12 @@ def muestra_imagenes(imagen, im_predicc, puntoA, puntoB):
     fig.add_subplot(1, 3, 3)
     plt.title("Radiografía con Recta")
     plt.imshow(imagen)
-    plt.plot([puntoA[1], puntoB[1]], [puntoA[0], puntoB[0]], color='r')
+    plt.plot([puntoA[1], puntos[0][1]], [puntoA[0], puntos[0][0]], color='r')
+    plt.plot([puntoB[1], puntos[-1][1]], [puntoB[0], puntos[-1][0]], color='r')
+    for i in range(len(puntos) - 1):
+        punto1 = puntos[i]
+        punto2 = puntos[i+1]
+        plt.plot([punto1[1], punto2[1]], [punto1[0], punto2[0]], color='r')  
     plt.axis('off')
 
     plt.show()
